927a8b5d22805b2ef736b08d85171cd817e5ce65
chso2 <chso2@naver.com>
2022-04-28 02:15:35 +0900

    기본구조 - resolve

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("init");
+class Promise{
+    constructor(executor){
+        this.callbacks = [];
+
+        try{
+            executor(this.onResolve.bind(this), this.onReject.bind(this));
+        }catch(err){
+            
+        }finally{
+
+        }
+    }
+    onResolve(val){
+        this.callbacks.forEach((callback) => {
+            val = callback(val);
+        })
+    }
+    onReject(){
+
+    }
+    then(){
+
+    }
+    catch(){
+
+    }
+    finally(){
+        
+    }
+}
