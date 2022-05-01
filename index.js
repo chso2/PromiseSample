5db3822a3fbef488d540f36a50cdf6eb632ca261
chso2 <chso2@naver.com>
2022-05-01 18:35:18 +0900

    onResolve, reject callbacks 에서 분기

--- index.js
+++ index.js
@@ -1,30 +1,56 @@
+const PENDING = 'PENDING',
+     REJECTED = 'REJECTED',
+    FULFILLED = 'FULFILLED';
 class Promise{
     constructor(executor){
+        this.state = PENDING;
         this.callbacks = [];
+        this.result = null;
+        this.errorHandler = () =>{}
+        this.finallyHandler = () =>{}
 
         try{
             executor(this.onResolve.bind(this), this.onReject.bind(this));
         }catch(err){
-            
+            this.errorHandler(err);
         }finally{
-
+            this.finallyHandler();
         }
     }
     onResolve(val){
-        this.callbacks.forEach((callback) => {
-            val = callback(val);
-        })
-    }
-    onReject(){
+        if(this.state === PENDING){
+            this.state = FULFILLED;
+            this.result = val;
 
+            setTimeout(() => {
+                this.callbacks.forEach((cb) => {
+                    cb.resolvedCb(val);
+                })
+            })
+        }
+        this.finallyHandler();
     }
-    then(){
+    onReject(err){
+        if(this.state === PENDING){
+            this.state = REJECTED;
+            this.result = val;
 
+            setTimeout(() => {
+                this.callbacks.forEach((cb) => {
+                    cb.rejectedCb(val);
+                })
+            })
+        }
     }
-    catch(){
-
-    }
-    finally(){
+    then(fn){
         
+    }
+    catch(fn){
+        this.errorHandler = fn;
+        return this;
+    }
+    finally(fn){
+        this.finallyHandler = fn
+        return this
     }
 }
