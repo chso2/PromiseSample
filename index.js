a432e5432065bfb5dd665bc9271cc1d0264c251a
chso2 <chso2@naver.com>
2022-05-01 20:32:42 +0900

    prototype 추가

--- index.js
+++ index.js
@@ -6,15 +6,13 @@ class Promise{
         this.state = PENDING;
         this.callbacks = [];
         this.result = null;
-        this.errorHandler = () =>{}
-        this.finallyHandler = () =>{}
 
         try{
             executor(this.onResolve.bind(this), this.onReject.bind(this));
         }catch(err){
-            this.errorHandler(err);
+            reject(err);
         }finally{
-            this.finallyHandler();
+
         }
     }
     onResolve(val){
@@ -28,29 +26,86 @@ class Promise{
                 })
             })
         }
-        this.finallyHandler();
     }
     onReject(err){
         if(this.state === PENDING){
             this.state = REJECTED;
-            this.result = val;
+            this.result = err;
 
             setTimeout(() => {
                 this.callbacks.forEach((cb) => {
-                    cb.rejectedCb(val);
+                    cb.rejectedCb(err);
                 })
             })
         }
     }
-    then(fn){
-        
+    then(resolvedCb, rejectedCb){
+        const that = this
+        resolvedCb = typeof resolvedCb === 'function' ? resolvedCb : (val) => val;
+        rejectedCb = typeof rejectedCb === 'function' ? rejectedCb : (err) => { throw err }
+
+        return new Promise((resolve, reject) => {
+            function callback(type){
+                try{
+                    let promiseResult = type(that.result);
+                    if(promiseResult instanceof Promise){
+                        promiseResult.then((v) => {
+                            resolve(v);
+                        }, (e) => {
+                            reject(e);
+                        });
+                    }else{
+                        resolve(promiseResult);
+                    }
+                }catch(err){
+                    reject(err);
+                }
+            }
+            if(this.state === FULFILLED){
+                setTimeout(() => {
+                    callback(resolvedCb);
+                });
+            }
+            if(this.state === REJECTED){
+                setTimeout(() => {
+                    callback(rejectedCb);
+                });
+            }
+            if(this.state === PENDING){
+                this.callbacks.push({
+                    resolvedCb : () => {
+                        callback(resolvedCb)
+                    },
+                    rejectedCb : () => {
+                        callback(rejectedCb);
+                    },
+                })
+            }
+        })
     }
+    // rejectedCb
     catch(fn){
-        this.errorHandler = fn;
-        return this;
+        return this.then(null, fn);
     }
-    finally(fn){
-        this.finallyHandler = fn
-        return this
+
+    // finally(fn){
+    //     this.finallyHandler = fn
+    //     return this
+    // }
+
+
+    static resolve(val){
+        return new Promise((resolve, reject) => {
+            if(val instanceof Promise){
+                val.then((v) => resolve(v), (e) => reject(e));
+            }else{
+                resolve(val);
+            }
+        });
+    }
+    static reject(err){
+        return new Promise((resolve, reject) => {
+            reject(err);
+        })
     }
 }
